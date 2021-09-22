45c3f63c04c2c28d01293873ef4fd38dddb94742
Marco Zullich <marco.zullich@gmail.com>
2021-09-22 21:26:18 +0200

    does it work?

--- src/nn/network.py
+++ src/nn/network.py
@@ -13,7 +13,10 @@ class Network(ABC):
 
     def eval_position(self, board_state):
         input = self._board_states_to_inputs([board_state])
+        # print(f"Input\n{input}")
+        # exit()
         result = self.model.predict(input)
+        # print(f"Position evaluated at {result}")
         return result
 
     def update(self, board_states, rewards):
@@ -22,16 +25,18 @@ class Network(ABC):
         self.model.train_on_batch(inputs, outputs)
 
     def load(self):
-        model_path = os.path.join(save_dir, self.get_save_file())
-        if os.path.isfile(model_path):
-            self.model = load_model(model_path)
-            print('Loaded model from', model_path)
+        pass
+        # model_path = os.path.join(save_dir, self.get_save_file())
+        # if os.path.isfile(model_path):
+        #     self.model = load_model(model_path)
+        #     print('Loaded model from', model_path)
     
     def save(self):
-        if not os.path.isdir(save_dir):
-            os.makedirs(save_dir)
-        model_path = os.path.join(save_dir, self.get_save_file())
-        self.model.save(model_path)
+        pass
+        # if not os.path.isdir(save_dir):
+        #     os.makedirs(save_dir)
+        # model_path = os.path.join(save_dir, self.get_save_file())
+        # self.model.save(model_path)
 
     def _board_states_to_inputs(self, board_states):
         inputs = np.array(board_states)
